83d5ecae1bc7bb385e350aae77f3cf884ea25413
vaibhav kohli <vaibhav.khl@gmail.com>
2018-10-16 22:30:01 +0530

    flask cors support & basic api call from client

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,54 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.setNumberOfRecords = this.setNumberOfRecords.bind(this);
+    this.createRecords = this.createRecords.bind(this)
+
+    this.state = {
+      numberOfRecords: 0
+    };
+  }
+
+  setNumberOfRecords(n) {
+    this.setState({
+      numberOfRecords: n
+    })
+  }
+
+  createRecords() {
+    let numberOfRecords = this.state.numberOfRecords
+    let url = 'http://ec2-18-222-209-0.us-east-2.compute.amazonaws.com:5000/user'
+    callApi(url,  {
+      method: 'POST', // or 'PUT'
+      body: JSON.stringify({
+        username: 'vaibhavkhl',
+        email: 'vaibhav.khl@gmail.com',
+        n: numberOfRecords
+      }), // data can be `string` or {object}!
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(resp => console.log(resp))
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input type="text" value={this.state.numberOfRecords} onChange={(e) => this.setNumberOfRecords(e.target.value)}/>
+        <button onClick={this.createRecords}>Create Records</button>
       </div>
     );
   }
 }
 
 export default App;
+
+function callApi(url, options) {
+    return fetch(url, options).then((resp) => {
+      return resp.json().then(data => {
+        return data
+      })
+    })
+  }
--- server/crud.py
+++ server/crud.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 import os
 import time
 
 app = Flask(__name__)
+CORS(app)
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/flask_react' #+os.path.join(basedir, 'crud.sqlite')
 db = SQLAlchemy(app)
